7bb97625fba23e9cb7858c7c054d3313010e4d27
Monir Cederstrom <32554823+monircederstrom@users.noreply.github.com>
2018-05-09 19:05:44 -0500

    fixed server.js

--- server.js
+++ server.js
@@ -26,10 +26,10 @@ var exphbs = require("express-handlebars");
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// Import routes and give the server access to them.
-var routes = require("./controllers/burgers_controller.js");
-
-
+// Routes
+// =============================================================
+require("./routes/api-routes.js")(app);
+require("./routes/html-routes.js")(app);
 
 //
 db.sequelize.sync({ force: true}).then(function() {
